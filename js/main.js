6918d878b6fefe63773f5160dff91cadc679bd6c
Felipe Milagres <felipemilagres.x@gmail.com>
2015-02-01 23:37:55 -0200

    css update

--- js/main.js
+++ js/main.js
@@ -1,24 +1,6 @@
 $(window).load(function(){
-   /* var progressbar = $('.progressbar'),  
-        max = 80,  
-        time = (1000/100)*1.5,      
-        value = progressbar.val();
-  
-    var loading = function() {  
-        value += 1;  
-        addValue = progressbar.val(value);  
-  
-        if (value == max) {  
-            clearInterval(animate);                      
-        }  
-    };  
-  
-    var animate = setInterval(function() {  
-        loading();  
-    }, time); */
-    
+
     $('.progressbar').each(function() {
-        console.log( $(this).data('current') );
         
         var progressbar = $(this),
             max = progressbar.data('current'),
